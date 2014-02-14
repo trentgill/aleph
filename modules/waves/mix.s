78c1665e165ee613c8234fa576f8bf055516a53b
emb <emb@catfact.net>
2014-02-14 00:49:05 -0800

    breaking...

--- modules/waves/mix.s
+++ modules/waves/mix.s
@@ -1,36 +1,74 @@
-.file "mix.s";
+.file "mix_test.c";
 .text;
 	.align 4
-	
 .global _mix_mod;
 .type _mix_mod, STT_FUNC;
 _mix_mod:
 	LINK 0;
-	P2 = R0			;; // pointer to input array
-	P1 = R1			;; // pointer to output array
+	[FP+8] = R0;
+	[FP+12] = R1;
+	[FP+16] = R2;
+	R0 = [FP+8];
+	P2 = R0;
+	P2 += 4;
+	R0 = [P2];
+	P2 = [FP+12];
+	[P2] = R0;
+	R0 = [FP+12];
+	P1 = R0;
+	P1 += 4;
+	P2 = [FP+8];
+	R0 = [P2];
+	[P1] = R0;
 	UNLINK;
 	rts;
 	.size	_mix_mod, .-_mix_mod
 	.align 4
 	
 .global _mix_voice;
-.type _mix_voice, STT_FUNC;	
+.type _mix_voice, STT_FUNC;
 _mix_voice:
 	LINK 0
-	[--sp] = (r7:6) 	;
-	// input and output arrays
-	p2 = r0			; 
-	p1 = r1 		; 
-	// sum the 2 input values
-	r7 = [p2++]		;
-	r6 = [p2]
-	r7 = r7 + r6 (S) 	; // add 2nd value
-	// store in all outputs
-	[p1++] = r7		; 
-	[p1++] = r7		; 
-	[p1++] = r7		; 
-	[p1] = r7		;
-	(r7:6) = [sp++] 	;
+	// i/o arrays
+	p1 = r0			;
+	p2 = r1 		;
+	// sum of first 2 inputs
+	r0 = [p1]		;
+//	r1 = [p1 + 4]		;
+//	r2 = r0 + r1 (S)	;
+	r2 = r0			;
+
+	/// WTFFF
+
+	// assign to each output
+	[p2++] = r0		;
+	[p2++] = r0		;
+	[p2++] = r0		;
+	[p2] = r0		;
+	
+	;; // add to each output
+	;; r0 = [p2]		;
+	;; r1 = r2 + r0 (S) 	;
+	;; [p2++] = r1		;
+	;; r0 = [p2]		;
+	;; r1 = r2 + r0 (S) 	;
+	;; [p2++] = r1		;
+	;; r0 = [p2]		;
+	;; r1 = r2 + r0 (S) 	;
+	;; [p2++] = r1		;
+	;; r0 = [p2]		;
+	;; r1 = r2 + r0 (S) 	;
+	;; [p2] = r1		;
+	
+	;; // loop over outputs
+	;; p3 = 4 			;
+	;; loop output lc0=p3	;
+	;; loop_begin output		;
+	;; 	r0 = [p2]		;
+	;; 	r1 = r0 + r2 (S)	;
+	;; 	[p2++] = r1	;
+	;; loop_end output 	;
+	
 	UNLINK;
 	rts;
 	.size	_mix_voice, .-_mix_voice
@@ -40,7 +78,62 @@ _mix_voice:
 .type _mix_adc, STT_FUNC;
 _mix_adc:
 	LINK 32;
-
+	[FP+8] = R0;
+	[FP+12] = R1;
+	[FP+16] = R2;
+	P2 = [FP+12];
+	R1 = [P2];
+	P2 = [FP+8];
+	R0 = [P2];
+	[FP+-28] = R1;
+	[FP+-32] = R0;
+	R1 = [FP+-28];
+	R0 = [FP+-32];
+	R0 = R1 + R0 (S);
+	P2 = [FP+12];
+	[P2] = R0;
+	R0 = [FP+12];
+	R0 += 4;
+	[FP+12] = R0;
+	P2 = [FP+12];
+	R1 = [P2];
+	P2 = [FP+8];
+	R0 = [P2];
+	[FP+-20] = R1;
+	[FP+-24] = R0;
+	R1 = [FP+-20];
+	R0 = [FP+-24];
+	R0 = R1 + R0 (S);
+	P2 = [FP+12];
+	[P2] = R0;
+	R0 = [FP+12];
+	R0 += 4;
+	[FP+12] = R0;
+	P2 = [FP+12];
+	R1 = [P2];
+	P2 = [FP+8];
+	R0 = [P2];
+	[FP+-12] = R1;
+	[FP+-16] = R0;
+	R1 = [FP+-12];
+	R0 = [FP+-16];
+	R0 = R1 + R0 (S);
+	P2 = [FP+12];
+	[P2] = R0;
+	R0 = [FP+12];
+	R0 += 4;
+	[FP+12] = R0;
+	P2 = [FP+12];
+	R1 = [P2];
+	P2 = [FP+8];
+	R0 = [P2];
+	[FP+-4] = R1;
+	[FP+-8] = R0;
+	R1 = [FP+-4];
+	R0 = [FP+-8];
+	R0 = R1 + R0 (S);
+	P2 = [FP+12];
+	[P2] = R0;
 	UNLINK;
 	rts;
 	.size	_mix_adc, .-_mix_adc
--- modules/waves/waves.c
+++ modules/waves/waves.c
@@ -136,10 +136,14 @@ static void calc_frame(void) {
   }
 
   // mix outputs
-  // out[0] = out[1] = out[2] = out[3] = 0;
-  mix_voice(voiceOut, out, (fract16**)mix_osc_dac);
-  mix_adc(in, out, (fract16**)mix_adc_dac);
-  //  mix_adc();	
+  out[0] = out[1] = out[2] = out[3] = 0;
+
+  //  out[0] = add_fr1x32(voiceOut[0], voiceOut[1]);
+
+  mix_voice((const fract32*)voiceOut, out, (fract16**)mix_osc_dac);
+
+
+  //  mix_adc(in, out, (fract16**)mix_adc_dac);
 
 #else
   /* wavesVoice* v; */
