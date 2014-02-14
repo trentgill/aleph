d795b6abb4ee3b7d515770ddb2b86a36f7eb4ee1
emb <emb@catfact.net>
2014-02-13 22:54:46 -0800

    trying hand asm for mix loops, works ok

--- modules/waves/mix.s
+++ modules/waves/mix.s
@@ -1,136 +1,46 @@
-.file "mix.c";
+.file "mix.s";
 .text;
 	.align 4
+	
 .global _mix_mod;
 .type _mix_mod, STT_FUNC;
 _mix_mod:
 	LINK 0;
-	[FP+8] = R0;
-	[FP+12] = R1;
-	[FP+16] = R2;
-	R0 = [FP+8];
-	P2 = R0;
-	P2 += 4;
-	R0 = [P2];
-	P2 = [FP+12];
-	[P2] = R0;
-	R0 = [FP+12];
-	P1 = R0;
-	P1 += 4;
-	P2 = [FP+8];
-	R0 = [P2];
-	[P1] = R0;
+	P2 = R0			;; // pointer to input array
+	P1 = R1			;; // pointer to output array
 	UNLINK;
 	rts;
 	.size	_mix_mod, .-_mix_mod
 	.align 4
+	
 .global _mix_voice;
-.type _mix_voice, STT_FUNC;
+.type _mix_voice, STT_FUNC;	
 _mix_voice:
-	LINK 12;
-	[FP+8] = R0;
-	[FP+12] = R1;
-	[FP+16] = R2;
-	P2 = [FP+8];
-	R0 = [P2];
-	[FP+-12] = R0;
-	R0 = [FP+8];
-	R0 += 4;
-	[FP+8] = R0;
-	P2 = [FP+8];
-	R1 = [P2];
-	R0 = [FP+-12];
-	[FP+-4] = R0;
-	[FP+-8] = R1;
-	R1 = [FP+-4];
-	R0 = [FP+-8];
-	R0 = R1 + R0 (S);
-	[FP+-12] = R0;
-	P2 = [FP+12];
-	R0 = [FP+-12];
-	[P2] = R0;
-	R0 = [FP+12];
-	R0 += 4;
-	[FP+12] = R0;
-	P2 = [FP+12];
-	R0 = [FP+-12];
-	[P2] = R0;
-	R0 = [FP+12];
-	R0 += 4;
-	[FP+12] = R0;
-	P2 = [FP+12];
-	R0 = [FP+-12];
-	[P2] = R0;
-	R0 = [FP+12];
-	R0 += 4;
-	[FP+12] = R0;
-	P2 = [FP+12];
-	R0 = [FP+-12];
-	[P2] = R0;
+	LINK 0
+	[--sp] = (r7:6) 	;
+	// input and output arrays
+	p2 = r0			; 
+	p1 = r1 		; 
+	// sum the 2 input values
+	r7 = [p2++]		;
+	r6 = [p2]
+	r7 = r7 + r6 (S) 	; // add 2nd value
+	// store in all outputs
+	[p1++] = r7		; 
+	[p1++] = r7		; 
+	[p1++] = r7		; 
+	[p1] = r7		;
+	(r7:6) = [sp++] 	;
 	UNLINK;
 	rts;
 	.size	_mix_voice, .-_mix_voice
 	.align 4
+	
 .global _mix_adc;
 .type _mix_adc, STT_FUNC;
 _mix_adc:
 	LINK 32;
-	[FP+8] = R0;
-	[FP+12] = R1;
-	[FP+16] = R2;
-	P2 = [FP+12];
-	R1 = [P2];
-	P2 = [FP+8];
-	R0 = [P2];
-	[FP+-28] = R1;
-	[FP+-32] = R0;
-	R1 = [FP+-28];
-	R0 = [FP+-32];
-	R0 = R1 + R0 (S);
-	P2 = [FP+12];
-	[P2] = R0;
-	R0 = [FP+12];
-	R0 += 4;
-	[FP+12] = R0;
-	P2 = [FP+12];
-	R1 = [P2];
-	P2 = [FP+8];
-	R0 = [P2];
-	[FP+-20] = R1;
-	[FP+-24] = R0;
-	R1 = [FP+-20];
-	R0 = [FP+-24];
-	R0 = R1 + R0 (S);
-	P2 = [FP+12];
-	[P2] = R0;
-	R0 = [FP+12];
-	R0 += 4;
-	[FP+12] = R0;
-	P2 = [FP+12];
-	R1 = [P2];
-	P2 = [FP+8];
-	R0 = [P2];
-	[FP+-12] = R1;
-	[FP+-16] = R0;
-	R1 = [FP+-12];
-	R0 = [FP+-16];
-	R0 = R1 + R0 (S);
-	P2 = [FP+12];
-	[P2] = R0;
-	R0 = [FP+12];
-	R0 += 4;
-	[FP+12] = R0;
-	P2 = [FP+12];
-	R1 = [P2];
-	P2 = [FP+8];
-	R0 = [P2];
-	[FP+-4] = R1;
-	[FP+-8] = R0;
-	R1 = [FP+-4];
-	R0 = [FP+-8];
-	R0 = R1 + R0 (S);
-	P2 = [FP+12];
-	[P2] = R0;
+
 	UNLINK;
 	rts;
 	.size	_mix_adc, .-_mix_adc
