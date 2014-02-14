ebf0af591729a85ef0ae18a9be9d59052d4e6c17
emb <emb@catfact.net>
2014-02-14 14:41:25 -0800

    devlopg and whatever

--- docs/devlog/index.php
+++ docs/devlog/index.php
@@ -2,34 +2,53 @@
 
 <head>
 
- <style type="text/css">
-.log {
-  white-space: pre
-  
+<style type="text/css">
+
+   .date {
+ font-weight:bold;
+ font-family:Consolas, Monaco, Courier, monospace;
+ background:#aaa;
+ width:666px;
+ padding:10px;
+ margin:10px;
+ margin-bottom:0px;
  }
- </style>
+   
+   .log {
+     font-family:"Helvetica Neue", "HelveticaNeueLT Std", Helvetica, Arial, sans-serif;
+     white-space: pre-wrap;
+ width:666px;
+ background:#ddd;
+ padding:10px;
+ margin:10px;
+ margin-top:0px;
+    }
+   </style>
 
-</head>
+   </head>
 
-<body>
+   <body>
 
-<?php
+ <?php
 
 
-$logdir = opendir('logs');
+   $logdir = opendir('logs');
 while (false !== ($log = readdir($logdir))) {
   if (strlen($log) > 2) {
     $logs[] = $log;
-}
+  }
 }
 
 sort($logs);
 
 foreach ($logs as $thislog) {
-  echo "<div class=\"log\">";
+  echo "<div class=\"date\">";
   echo "<h>".$thislog."</h>";
+  echo "</div>";
+  echo "<div class=\"log\">";
   echo file_get_contents("logs/".$thislog);
   echo "</div>";
+
 }
 	
 
